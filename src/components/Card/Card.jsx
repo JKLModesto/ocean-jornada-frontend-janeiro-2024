5f91f63742a4c3b3778d3cf838c855c505d10f3c
JKLModesto <pedrogabrielmodesto@gmail.com>
2024-01-31 12:06:58 -0300

    Formatando

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,25 @@
 /* eslint-disable react/prop-types */
 function Card(props) {
+  const item = props.item;
 
-  const item = props.item
+  console.log(item);
 
-  console.log(item)
-
-    return (
-        <>
-          <div className="card">
-            <h2>{item.name}</h2>
-          <div>
-            <div className="tags">
+  return (
+    <>
+      <div className="card">
+        <h2>{item.name}</h2>
+        <div>
+          <div className="tags">
             <div className="tag">Status: Vivo</div>
             <div className="tag">Espécie: Humana</div>
             <div className="tag">Origem: Terra C-137</div>
-            </div>
-            </div>
-
-            <img className="img" src={item.image} />
           </div>
-        </>
-      )
+        </div>
+
+        <img className="img" src={item.image} />
+      </div>
+    </>
+  );
 }
 
-export default Card
+export default Card;
